90cace50ac16cec832b5a83d8bb314e80b5e01e6
JSchrtke <johannes.schroetke@eddymax.com>
2019-11-14 12:31:30 +0100

    Add print and order methds & refactor

--- pdf_merge.py
+++ pdf_merge.py
@@ -18,6 +18,7 @@ class PdfList:
 
     def __next__(self):
         if self._counter > len(self._list) - 1:
+            self._counter = 0
             raise StopIteration
 
         pdf_path = self._list[self._counter]
@@ -27,15 +28,49 @@ class PdfList:
     def __len__(self):
         return len(self._list)
 
+    def print(self):
+        for index, path in enumerate(self._list):
+            print(f"{index}: {path}")
 
-if __name__ == "__main__":
-    path_to_pdfs = input("Enter path to directory containing pdf files: ")
-    pdf_list = PdfList(path_to_pdfs)
+    def order(self):
+        print("\nCurrent order:")
+        self.print()
+        print("\nNew order:")
+        new_order = []
+        for i, _ in enumerate(self._list):
+            new_index = input(f"{i}: ")
+            if new_index == "x":
+                pass
+            else:
+                new_order.append(self._list[int(new_index)])
+        print("\n")
+        self._list = new_order
 
-    output_file = os.path.join(path_to_pdfs, "output.pdf")
+
+def merge(pdf_list, output_file):
     print(f"Merging into {output_file}")
-    
     merger = PdfFileMerger()
     for pdf in pdf_list:
         merger.append(pdf)
     merger.write(output_file)
+
+
+def is_ordered(pdf_list):
+    pdf_list.print()
+    choice = input("Is this order okay? (y/n)\n")
+    if choice == "y":
+        return True
+    else:
+        return False
+
+
+if __name__ == "__main__":
+    path_to_pdfs = input("Enter path to directory containing pdf files:\n")
+    pdf_list = PdfList(path_to_pdfs)
+
+    while not is_ordered(pdf_list):
+        pdf_list.order()
+    
+    output_name = input("Enter name for the ouput file:\n")
+    output_file = os.path.join(path_to_pdfs, f"{output_name}.pdf")
+    merge(pdf_list, output_file)
